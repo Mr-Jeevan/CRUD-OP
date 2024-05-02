f50045637f3830b4ce847a1d08b58e8da52e4a27
Mr-Jeevan <jeevee.a77@gmail.com>
2024-05-02 16:22:44 +0530

    add bcrypt reinst

--- api-server/users.js
+++ api-server/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const usersModal = require("./usersschema");
 const { ObjectId } = require('mongodb');
 //multer
