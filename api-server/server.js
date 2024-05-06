131bff0a9f5888e6e24dff4f452b8f1c1e6b4e3b
Mr-Jeevan <jeevee.a77@gmail.com>
2024-05-06 21:14:56 +0530

    atlas giving acces to all ip address

--- api-server/server.js
+++ api-server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 require('dotenv').config()
+require('dotenv').config
 const bodyParser = require('body-parser');
 var cors = require('cors')
 const app = express()
